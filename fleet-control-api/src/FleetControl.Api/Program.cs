068213ccbc487d7c248440eb09b515ef5bd35ad2
Joao Alonso <joao.vitoralonso@gmail.com>
2026-06-16 21:37:15 -0300

    arquivos vite

--- fleet-control-api/src/FleetControl.Api/Program.cs
+++ fleet-control-api/src/FleetControl.Api/Program.cs
@@ -28,7 +28,7 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(
                 builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>()
-                ?? ["http://localhost:5173", "http://localhost:3000"])
+                ?? ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000"])
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
